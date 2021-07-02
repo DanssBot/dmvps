03e71089669a47c241172be7fc908c150d221e7c
cavalieri22 <75698303+cavalieri22@users.noreply.github.com>
2021-07-02 11:16:09 -0500

    Add files via upload

---
+++ BotGenADM1.8/repos/BotGen.sh
@@ -0,0 +1,147 @@
+#!/bin/bash
+
+check_ip () {
+MIP=$(ip addr | grep 'inet' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -o -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | head -1)
+MIP2=$(wget -qO- ipv4.icanhazip.com)
+[[ "$MIP" != "$MIP2" ]] && IP="$MIP2" || IP="$MIP"
+echo "$IP" > /usr/bin/vendor_code
+}
+
+function_verify () {
+  permited=$(curl -sSL "https://raw.githubusercontent.com/rudi9999/Control/master/Control-Bot")
+  [[ $(echo $permited|grep "${IP}") = "" ]] && {
+  clear
+  echo -e "\n\n\n\e[31m====================================================="
+  echo -e "\e[31m      ¡LA IP $(wget -qO- ipv4.icanhazip.com) NO ESTA AUTORIZADA!\n     SI DESEAS USAR EL BOTGEN CONTACTE A @Rufu99"
+  echo -e "\e[31m=====================================================\n\n\n\e[0m"
+  exit 1
+  } || {
+  ### INTALAR VERCION DE SCRIPT
+  v1=$(curl -sSL "https://raw.githubusercontent.com/rudi9999/TeleBotGen/master/Vercion")
+  echo "$v1" > /etc/ADM-db/vercion
+  }
+}
+
+#check_ip
+#function_verify
+  
+CIDdir=/etc/ADM-db && [[ ! -d ${CIDdir} ]] && mkdir ${CIDdir}
+SRC="${CIDdir}/sources" && [[ ! -d ${SRC} ]] && mkdir ${SRC}
+CID="${CIDdir}/User-ID" && [[ ! -e ${CID} ]] && echo > ${CID}
+keytxt="${CIDdir}/keys" && [[ ! -d ${keytxt} ]] && mkdir ${keytxt}
+[[ $(dpkg --get-selections|grep -w "jq"|head -1) ]] || apt-get install jq -y &>/dev/null
+[[ ! -e "/bin/ShellBot.sh" ]] && wget -O /bin/ShellBot.sh https://raw.githubusercontent.com/rudi9999/TeleBotGen/master/ShellBot.sh &> /dev/null
+[[ -e /etc/texto-bot ]] && rm /etc/texto-bot
+LINE="==========================="
+
+# Importando API
+source ShellBot.sh
+source ${SRC}/menu
+source ${SRC}/ayuda
+source ${SRC}/cache
+source ${SRC}/invalido
+source ${SRC}/status
+source ${SRC}/reinicio
+source ${SRC}/myip
+source ${SRC}/id
+source ${SRC}/back_ID
+source ${SRC}/link
+source ${SRC}/listID
+source ${SRC}/gerar_key
+source ${SRC}/power
+source ${SRC}/comandos
+source ${SRC}/update
+
+# Token del bot
+bot_token="$(cat ${CIDdir}/token)"
+
+# Inicializando el bot
+ShellBot.init --token "$bot_token" --monitor --return map
+ShellBot.username
+
+download_file () {
+# shellbot.sh editado linea 3986
+user=User-ID
+[[ -e ${CID} ]] && rm ${CID}
+local file_id
+          ShellBot.getFile --file_id ${message_document_file_id[$id]}
+          ShellBot.downloadFile --file_path "${return[file_path]}" --dir "${CIDdir}"
+local bot_retorno="ID user botgen\n"
+		bot_retorno+="$LINE\n"
+		bot_retorno+="Se restauro con exito!!\n"
+		bot_retorno+="$LINE\n"
+		bot_retorno+="${return[file_path]}\n"
+		bot_retorno+="$LINE"
+			ShellBot.sendMessage	--chat_id "${message_chat_id[$id]}" \
+									--reply_to_message_id "${message_message_id[$id]}" \
+									--text "<i>$(echo -e $bot_retorno)</i>" \
+									--parse_mode html
+return 0
+}
+
+msj_add () {
+	      ShellBot.sendMessage --chat_id ${1} \
+							--text "<i>$(echo -e $bot_retor)</i>" \
+							--parse_mode html
+}
+
+upfile_fun () {
+          ShellBot.sendDocument --chat_id ${message_chat_id[$id]}  \
+                             --document @${1}
+}
+
+invalido_fun () {
+[[ ! -z ${callback_query_message_chat_id[$id]} ]] && var=${callback_query_message_chat_id[$id]} || var=${message_chat_id[$id]}
+local bot_retorno="  🎊 Bienvenido al bot de keys  🎊\n"
+	 bot_retorno+="$LINE\n"
+         bot_retorno+="𝙲𝚘𝚖𝚊𝚗𝚍𝚘 𝙸𝚗𝚟𝚊𝚕𝚒𝚍𝚘!! 𝚃𝚎𝚌𝚕𝚎𝚊 /𝚊𝚢𝚞𝚍𝚊 𝚙𝚊𝚛𝚊 𝙲𝚘𝚗𝚘𝚌𝚎𝚛 𝚕𝚘𝚜 𝙿𝚊𝚜𝚘𝚜! \n O Contacta a $(cat < /etc/ADM-db/resell) \n"
+         bot_retorno+="$LINE\n"
+	     ShellBot.sendMessage --chat_id $var \
+							--text "<i>$(echo -e $bot_retorno)</i>" \
+							--parse_mode html
+	return 0	
+}
+
+msj_fun () {
+	      ShellBot.sendMessage --chat_id ${message_chat_id[$id]} \
+							--text "<i>$(echo -e "$bot_retorno")</i>" \
+							--parse_mode html
+	return 0
+}
+
+reply () {
+		  ShellBot.sendMessage	--chat_id ${message_from_id[$id]} \
+							--text "$bot_retorno" \
+							--reply_markup "$(ShellBot.ForceReply)"
+}
+
+
+botao_conf=''
+botao_user=''
+botao_donar=''
+
+ShellBot.InlineKeyboardButton --button 'botao_conf' --line 1 --text 'Añadir' --callback_data '/add'
+ShellBot.InlineKeyboardButton --button 'botao_conf' --line 1 --text 'Del' --callback_data '/del'
+ShellBot.InlineKeyboardButton --button 'botao_conf' --line 1 --text 'List' --callback_data '/list'
+ShellBot.InlineKeyboardButton --button 'botao_conf' --line 1 --text 'ID' --callback_data '/ID'
+
+ShellBot.InlineKeyboardButton --button 'botao_conf' --line 2 --text 'ON/OFF' --callback_data '/power'
+ShellBot.InlineKeyboardButton --button 'botao_conf' --line 2 --text 'MENU' --callback_data '/menu'
+
+ShellBot.InlineKeyboardButton --button 'botao_conf' --line 3 --text 'GENERAR KEY' --callback_data '/keygen'
+ShellBot.InlineKeyboardButton --button 'botao_user' --line 1 --text 'GENERAR KEY' --callback_data '/keygen'
+
+ShellBot.InlineKeyboardButton --button 'botao_donar' --line 1 --text 'Donar Paypal' --callback_data '1' --url 'https://www.paypal.me/'
+ShellBot.InlineKeyboardButton --button 'botao_donar' --line 3 --text 'ACCEDER WHATSAPP' --callback_data '1' --url 'https://wa.me/529842147259'
+# Ejecutando escucha del bot
+while true; do
+    ShellBot.getUpdates --limit 100 --offset $(ShellBot.OffsetNext) --timeout 30
+    for id in $(ShellBot.ListUpdates); do
+	    chatuser="$(echo ${message_chat_id[$id]}|cut -d'-' -f2)"
+	    echo $chatuser >&2
+	    comando=(${message_text[$id]})
+	    [[ ! -e "${CIDdir}/Admin-ID" ]] && echo "null" > ${CIDdir}/Admin-ID
+	    permited=$(cat ${CIDdir}/Admin-ID)
+	    comand
+    done
+done
